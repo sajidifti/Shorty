cbf829c7f70d835983ca910772e7429de7aa7624
Sajid Anam Ifti <info.sajidifti@gmail.com>
2024-02-14 23:12:08 +0600

    redirect view + fixed forms

--- main/views.py
+++ main/views.py
@@ -1,41 +1,26 @@
 from django.contrib.auth.decorators import login_required
 from users.decorators import admin_only
 from django.shortcuts import render, redirect, get_object_or_404
-
-import random
-import string
+from django.http import Http404
 from django.contrib import messages
 from .forms import URLShortenerForm
 from .models import URLShortener
 
 
-def generate_random_custom_url(length=6):
-    """Generate a random alphanumeric string."""
-    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
-
-
+@login_required
 def home(request):
     allerrors = ""
 
     if request.method == "POST":
         form = URLShortenerForm(request.POST)
         if form.is_valid():
-            custom_url = form.cleaned_data["custom_url"]
 
-            if not custom_url:
-                custom_url = generate_random_custom_url()
+            url_shortener = form.save(commit=False)
+            url_shortener.user = request.user
+            url_shortener.save()
 
-                while URLShortener.objects.filter(custom_url=custom_url).exists():
-                    custom_url = generate_random_custom_url()
-
-            else:
-                url_shortener = form.save(commit=False)
-                url_shortener.user = request.user
-                url_shortener.custom_url = custom_url
-                url_shortener.save()
-
-                messages.success(request, "URL shortened successfully.")
-                return redirect("shortened", pk=url_shortener.pk)
+            messages.success(request, "URL shortened successfully.")
+            return redirect("shortened", pk=url_shortener.pk)
         else:
             for field, error_messages in form.errors.items():
                 for error_message in error_messages:
@@ -47,18 +32,51 @@ def home(request):
     return render(request, "main/index.html", {"form": form})
 
 
-@login_required
-def myurls(request):
-    return render(request, "main/myURLs.html")
+def redirect_to_original(request, custom_url):
+    try:
+        url_shortener = URLShortener.objects.get(custom_url=custom_url)
+        return redirect(url_shortener.original_url)
+    except URLShortener.DoesNotExist:
+        raise Http404("Shortened URL does not exist.")
 
 
 @login_required
 @admin_only
 def allurls(request):
-    return render(request, "main/allURLs.html")
+    if request.method == "POST":
+        url_id = request.POST.get("url_id")
+        url_shortener = get_object_or_404(URLShortener, pk=url_id)
+        url_shortener.delete()
+        messages.success(request, "URL deleted successfully.")
+        return redirect("allurls")
+
+    urls = URLShortener.objects.all()
+    return render(request, "main/allURLs.html", {"urls": urls})
+
+
+@login_required
+def myurls(request):
+    user = request.user
+
+    if request.method == "POST":
+        url_id = request.POST.get("url_id")
+        url_shortener = get_object_or_404(URLShortener, pk=url_id)
+        url_shortener.delete()
+        messages.success(request, "URL deleted successfully.")
+        return redirect("myurls")
+
+    urls = URLShortener.objects.filter(user=user)
+
+    return render(request, "main/myURLs.html", {"urls": urls})
 
 
 @login_required
 def shortened(request, pk):
+    user = request.user
     url_shortener = get_object_or_404(URLShortener, pk=pk)
+    url_user = url_shortener.user
+
+    if user != url_user:
+        return redirect("home")
+
     return render(request, "main/shortened.html", {"url_shortener": url_shortener})
